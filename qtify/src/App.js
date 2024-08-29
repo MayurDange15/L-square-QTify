0818ffab704ef9b2449ba8affff79325fe2350d3
MayurDange15 <68801382+MayurDange15@users.noreply.github.com>
2024-08-29 10:11:08 +0530

    Milestone 2

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,14 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import Logo from "./components/Logo/Logo";
 import Navbar from "./components/Navbar/Navbar";
 import Search from "./components/Search/Search";
 import { Outlet } from "react-router-dom";
-
+import { fetchTopAlbum } from "./components/api/api";
 function App() {
+  const [data, setData] = useState({});
+
+  const generateData = (key, dataSource) => {
+    dataSource().then((data) => {
+      setData((prevData) => {
+        return { ...prevData, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbum", fetchTopAlbum);
+  }, []);
+
+  const { topAlbum = [] } = data;
+
   return (
     <>
-      <Navbar>
+      <Navbar searchData={[...topAlbum]}>
         <Logo />
         <Search placeH={"Search a album of your choice"} />
         <Button
@@ -16,7 +33,7 @@ function App() {
           onClick={() => alert("Feedback button clicked!")}
         />
       </Navbar>
-      <Outlet />
+      <Outlet context={{ data: { topAlbum } }} />
     </>
   );
 }
--- qtify/src/pages/HomePage/HomePage.jsx
+++ qtify/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import HeroSession from "../../components/Hero/Hero";
-
-export default function HomePage(){
-    return (
-        <>
-        <HeroSession/>
-        </>
-    )
+import styles from "./HomePage.module.css";
+import Hero from "../../components/Hero/Hero";
+import { useOutletContext } from "react-router-dom";
+import Section from "../../components/Section/Section";
+export default function HomePage() {
+  const { data } = useOutletContext();
+  const { topAlbum } = data;
+  return (
+    <>
+      <Hero />
+      <div className={styles.section_wrapper}>
+        <Section title={"Top Album"} data={topAlbum} type={"album"} />
+      </div>
+    </>
+  );
 }
